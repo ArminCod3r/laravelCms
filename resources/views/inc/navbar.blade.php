7258892dce3398ecdf74beb5129e669e4c2b1d2d
ArminCod3r <Armin.MNSR@gmail.com>
2019-08-31 09:19:21 -0700

    about page added

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,13 +8,13 @@
                 <span class="icon-bar">  </span>
             </button>
             
-            <a class="navbar-brand" href="/"> Project Name </a>
+            <a class="navbar-brand" href="/"> Laravel CMS </a>
         </div>
         
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#"> Home </a></li>
-                <li><a href="#"> About </a></li>
+                <li><a href="/about"> About </a></li>
                 <li><a href="#"> Post </a></li>
             </ul>
         </div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout/app')
+
+@section('content')
+
+        <h1> About </h1>
+        <p> Our address is: ...</p>
+        <p> Our phone is: ...</p>
+
+@endsection
