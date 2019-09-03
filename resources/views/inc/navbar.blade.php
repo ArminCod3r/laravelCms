b002f255023a6f66e3078bbf784929d96956e4b8
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-03 00:31:36 -0700

    navbar: /posts (added)

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -15,7 +15,7 @@
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#"> Home </a></li>
                 <li><a href="/about"> About </a></li>
-                <li><a href="#"> Post </a></li>
+                <li><a href="/posts"> Post </a></li>
             </ul>
         </div>
     </div>
