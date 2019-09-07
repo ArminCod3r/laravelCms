3fd9f3edf57b2f53b0fdec1f390aebd6d0ca8b64
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-07 08:39:39 -0700

    Use Relationship: number of posts each user has

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,10 +14,42 @@
                         </div>
                     @endif
 
-                    You are logged in!
-                    <br/><br/>                    
+                    welcome {{ Auth::user()->name }}
+                    <br/><br/>
 
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
+
+                    <h3>Your Posts</h3>
+                <hr />
+                
+                @if(count($posts) > 0)
+                    <table class="table table-striped">
+                    <tr>
+                        <th> Title </th>
+                       <th></th>
+                        <th></th>
+                    </tr>
+
+                    @foreach($posts as $item)
+                        <tr>
+                            <td> {{ $item->title }} </td>
+                            <td> <a href="posts/{{ $item->id }}/edit" class="btn btn-default"> Edit </a> </td>
+                            <td>
+                                <form action="{{route('posts.destroy', $item->id ) }}" method="POST" accept-charset="utf-8" class="pull-right">
+
+                                    {{ csrf_field() }}      
+                                    <input type="hidden" name="_method" value="DELETE"> 
+                                    <input type="submit" name="submit" value="DELETE" class="btn btn-danger">
+                                </form>
+                            </td>
+                        </tr>       
+                    @endforeach
+                    </table>
+
+                @else
+                    <p>You have no post</p>
+
+                @endif
 
                 </div>
             </div>
