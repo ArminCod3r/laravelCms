9405314f9dfd35593aa7964dd13290c503b60762
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-09 00:56:16 -0700

    Showing image alongside post (index page)

--- resources/views/postpages/index.blade.php
+++ resources/views/postpages/index.blade.php
@@ -8,8 +8,21 @@
 		@foreach($posts as $item)
 			<div class="well">
 
-				<h3> <a href="/posts/{{ $item->id }}"> {{ $item->title }} </a> </h3>
-				<small> Written on :{{ $item->created_at }} - By: {{ $item->user->name }} </small>
+				<div class="row">
+
+					<div class="col-md-4 col-sm-4">
+						<img style="width:80%" src="storage/cover_images/{{$item->cover_image}}">
+					</div>				
+				
+
+					<div class="col-md-8 col-sm-8">					
+						<h3> <a href="/posts/{{ $item->id }}"> {{ $item->title }} </a> </h3>
+						<small> Written on :{{ $item->created_at }} - By: {{ $item->user->name }} </small>
+					</div>
+
+				</div>
+
+				
 				
 			</div>			
 		@endforeach
